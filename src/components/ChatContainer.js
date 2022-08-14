c007671245ec164353574b28aa88720e6d6d1376
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2022-08-14 22:02:06 +0530

    js updated

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import {React, useState} from 'react'
 import './ChatContainer.css'
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import SendIcon from '@mui/icons-material/Send';
 import ChatMessage from './ChatMessage';
+import EmojiPicker from 'emoji-picker-react';
 
 function ChatContainer() {
+
+    const [msg, setmsg] = useState('')
+    const [openEmojiBox, setopenEmojiBox] = useState(false)
+
+
   return (
     <div className='chat-container'>
         <div className="chat-container-header">
@@ -32,13 +38,23 @@ function ChatContainer() {
         {/* chat input  */}
         <div className="chat-input">
             {/* buttons */}
+
+            {openEmojiBox && <EmojiPicker onEmojiClick={(event, emojiObject) => {
+                setmsg(msg + emojiObject.emoji)
+            }} />}
+
+
             <div className="chat-input-btn">
-                <InsertEmoticonIcon />
+                <InsertEmoticonIcon onClick={() => {
+                    setopenEmojiBox(!openEmojiBox)
+                }} />
                 <AttachFileIcon />
             </div>
             {/* text input */}
             <form>
-                <input type="text" placeholder='Type a message...' />
+                <input type="text" placeholder='Type a message...' value={msg} onChange={(e) => {
+                    setmsg(e.target.value)
+                }} />
             </form>
             {/* send btn */}
             <div className="chat-input-send-btn">
